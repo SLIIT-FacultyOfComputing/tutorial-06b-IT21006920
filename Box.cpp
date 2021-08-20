aa2109a20d369d38e2c98b03229ec52e92fdc1d2
IT21006920 <87510355+IT21006920@users.noreply.github.com>
2021-08-20 04:02:39 +0000

    Completed

--- Box.cpp
+++ Box.cpp
@@ -5,16 +5,34 @@ void Box::setlength(int l)
 {
   length = l;
 }
+
 void Box::setwidth(int w)
 {
   width = w;
 }
+
 void Box::setheight(int h)
 {
   height = h;
 }
 
+int Box::getLength()
+{
+  return length;
+}
+
+int Box::getHeight()
+{
+  return height;
+}
+
+int Box::getWidth()
+{
+  return width;
+}
 
 // Implemenet the calcVolume() unction
-int Box::calcVolume() {
+int Box::calcVolume() 
+{
+  return length*width*height;
 }
